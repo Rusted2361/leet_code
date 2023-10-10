24d3573eab85021d522a4112e4f520083a260eab
Hamza <hamzasajid4040@gmail.com>
2023-10-10 17:24:15 +0500

    added further test cases

--- giga_second/src/main.rs
+++ giga_second/src/main.rs
@@ -26,3 +26,28 @@ fn date() {
     let start_date = dt(2011, 4, 25, 0, 0, 0);
     assert_eq!(after(start_date), dt(2043, 1, 1, 1, 46, 40));
 }
+
+#[test]
+
+fn another_date() {
+    let start_date = dt(1977, 6, 13, 0, 0, 0);
+    assert_eq!(after(start_date), dt(2009, 2, 19, 1, 46, 40));
+}
+#[test]
+
+fn third_date() {
+    let start_date = dt(1959, 7, 19, 0, 0, 0);
+    assert_eq!(after(start_date), dt(1991, 3, 27, 1, 46, 40));
+}
+#[test]
+
+fn datetime() {
+    let start_date = dt(2015, 1, 24, 22, 0, 0);
+    assert_eq!(after(start_date), dt(2046, 10, 2, 23, 46, 40));
+}
+#[test]
+
+fn another_datetime() {
+    let start_date = dt(2015, 1, 24, 23, 59, 59);
+    assert_eq!(after(start_date), dt(2046, 10, 3, 1, 46, 39));
+}
