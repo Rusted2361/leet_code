e5e2e781aa4962355522a75b975a863253ced173
Hamza <hamzasajid4040@gmail.com>
2023-10-10 17:18:19 +0500

    added test case

--- giga_second/src/main.rs
+++ giga_second/src/main.rs
@@ -8,5 +8,21 @@ pub fn after(start: DateTime) -> DateTime {
 }
 
 fn main() {
-    let first_giga = after();
+    println!("helloworlds");
+}
+
+/// Create a datetime from the given numeric point in time.
+///
+/// Panics if any field is invalid.
+fn dt(year: i32, month: u8, day: u8, hour: u8, minute: u8, second: u8) -> DateTime {
+    use time::{Date, Time};
+    DateTime::new(
+        Date::from_calendar_date(year, month.try_into().unwrap(), day).unwrap(),
+        Time::from_hms(hour, minute, second).unwrap(),
+    )
+}
+#[test]
+fn date() {
+    let start_date = dt(2011, 4, 25, 0, 0, 0);
+    assert_eq!(after(start_date), dt(2043, 1, 1, 1, 46, 40));
 }
---
+++ giga_second/target/debug/deps/giga_seconf-6657645f3c6ea4ec.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/Desktop/Rust/leetcode/giga_second/target/debug/deps/giga_seconf-6657645f3c6ea4ec: src/main.rs
+
+/Users/macbookpro/Desktop/Rust/leetcode/giga_second/target/debug/deps/giga_seconf-6657645f3c6ea4ec.d: src/main.rs
+
+src/main.rs:
