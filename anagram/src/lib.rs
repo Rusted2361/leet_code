1df89da8dbf74d40215fdbc9cbe24e676945acce
Hamza <hamzasajid4040@gmail.com>
2023-10-15 14:59:31 +0500

    added mod to check multiple codes

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,29 +1,24 @@
-pub fn is_anagram(s: String, t: String) -> bool {
-    if s.len() != t.len() {
-        return false;
-    }
-    let mut s_chars: Vec<char> = s.chars().collect();
-    let mut t_chars: Vec<char> = t.chars().collect();
-
-    // Sort the character arrays
-    s_chars.sort();
-    t_chars.sort();
-
-    s_chars == t_chars
+pub fn anagram_string(s: String, t: String) {
+    anagraming::is_anagram_hashmap(s,t);
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_anagrams() {
-        assert_eq!(is_anagram("listen".to_string(), "silent".to_string()), true);
-        assert_eq!(is_anagram("hello".to_string(), "world".to_string()), false);
-        assert_eq!(is_anagram("abcdef".to_string(), "abcdefg".to_string()), false);
+mod anagraming{
+    pub fn is_anagram_sort(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+        let mut s_chars: Vec<char> = s.chars().collect();
+        let mut t_chars: Vec<char> = t.chars().collect();
+    
+        // Sort the character arrays
+        s_chars.sort();
+        t_chars.sort();
+    
+        s_chars == t_chars
     }
+    pub fn is_anagram_hashmap(s: String, t: String) -> bool {
+        let mut map = std::collections::HashMap::new();
+        s.chars().for_each(|c| *map.entry(c).or_insert(0) += 1); 
+        t.chars().for_each(|c| *map.entry(c).or_insert(0) -= 1); 
+        !map.into_values().any(|count| count != 0)
+     }
 }
-
-// fn main() {
-//     println!("Hello, world!");
-// }
