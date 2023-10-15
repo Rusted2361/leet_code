3bf7797eae8a193129fa6e23e4367848446be601
Hamza <hamzasajid4040@gmail.com>
2023-10-15 21:11:33 +0500

    added function for comparison

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,6 +2,7 @@ pub fn anagram_string(s: String, t: String) {
     anagraming::is_anagram_hashmap(s,t);
 }
 mod anagraming{
+    use std::collections::HashMap;
     pub fn is_anagram_sort(s: String, t: String) -> bool {
         if s.len() != t.len() {
             return false;
@@ -16,9 +17,20 @@ mod anagraming{
         s_chars == t_chars
     }
     pub fn is_anagram_hashmap(s: String, t: String) -> bool {
-        let mut map = std::collections::HashMap::new();
+        let mut map = HashMap::new();
         s.chars().for_each(|c| *map.entry(c).or_insert(0) += 1); 
         t.chars().for_each(|c| *map.entry(c).or_insert(0) -= 1); 
         !map.into_values().any(|count| count != 0)
      }
+     fn count_chars(s: String) -> HashMap::<char, i32> {
+        s.chars().fold(HashMap::<char, i32>::new(), |mut map, ch| {
+            map.entry(ch).and_modify(|count| *count += 1 ).or_insert(1);
+            map
+        })
+    }
+    
+    pub fn is_anagram_fold(s: String, t: String) -> bool {
+            count_chars(s) == count_chars(t)
+        }
+    
 }
